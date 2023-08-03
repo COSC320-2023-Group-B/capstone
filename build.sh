7882d7fe45ea409e40c95f773ff129153ab490ae
Zero_4793 <82554287+Zero4793@users.noreply.github.com>
2023-08-03 14:15:36 +1000

    Undo useless change

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 CFLAGS="-Wall -Wextra -pedantic"
 # CFLAGS+="Werror"
 
-g++ $CFLAGS -o ./build/main.exe ./src/main.cpp -Lctlibrary-windows-v1.6.5/Win64 -lcaretaker_static
+g++ $CFLAGS -o ./build/main.exe ./src/main.cpp
 
 # Stray Symbol: 
